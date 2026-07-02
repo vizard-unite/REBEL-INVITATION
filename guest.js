e8b84a6b67b71e9fe8a7c828aa37fa635aabad8a
muhadimulyana <muhammadadimulyana30@gmail.com>
2026-07-02 20:31:48 +0700

    update guest

--- guest.js
+++ guest.js
@@ -1,7 +1,8 @@
 const GUESTS = {
     "A8X91K": "ETERNAL GLORY (EG)",
     "P7LM2Q": "ENERGY MAJESTIC (ENMA)",
-    "C7ZB2K": "REBEL TEAM",
+    "Z23F9M": "Sugar Sparkles (§)",
+    "REBEL": "REBEL TEAM",
     "W5ZS8R": "Wish",
     "V9KD3N": "Vizard"
 };
